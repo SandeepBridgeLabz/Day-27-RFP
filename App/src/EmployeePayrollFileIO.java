f565b4f8996d16a6a29d23d7056b821605122c7f
sandeep <sandeepmishra898k@gmail.com>
2026-06-11 15:59:03 +0530

    UC4: Store Employee Payroll into File

---
+++ App/src/EmployeePayrollFileIO.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class EmployeePayrollFileIO {
+
+    private static final String PAYROLL_FILE =
+            "employee-payroll.txt";
+
+    public void writeData(List<EmployeePayrollData> employees)
+            throws IOException {
+
+        BufferedWriter writer =
+                new BufferedWriter(
+                        new FileWriter(PAYROLL_FILE));
+
+        for (EmployeePayrollData emp : employees) {
+            writer.write(emp.toString());
+            writer.newLine();
+        }
+
+        writer.close();
+    }
+}
